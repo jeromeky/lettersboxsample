9f6320e976669b4fdb7ec0305205eef7257dbb28
Jerome Ky <jeromeky@gmail.com>
2013-11-18 18:06:20 +1000

    commit

--- LettersBoxSample/src/main/java/com/arkanan/lettersboxsample/MainActivity.java
+++ LettersBoxSample/src/main/java/com/arkanan/lettersboxsample/MainActivity.java
@@ -16,7 +16,6 @@ import android.widget.Toast;
 import com.arkanan.lettersbox.factory.LettersBoxFactory;
 import com.arkanan.lettersbox.model.EnterLettersBoxStyle;
 import com.arkanan.lettersbox.model.LettersBoxArg;
-import com.arkanan.lettersboxsample.R;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,8 +27,6 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
         final Activity myActivity = this;
 
@@ -66,46 +63,6 @@ public class MainActivity extends ActionBarActivity {
 
             }
         }.construct();
-    }
-
-
-
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
     }
 
 }
